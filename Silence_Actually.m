42e43d62847b0c5ebdca81634c0704b18660b3f4
marisagrams <43827815+marisagrams@users.noreply.github.com>
2019-02-21 18:44:22 -0800

    Add files via upload

---
+++ Silence_Actually.m
@@ -0,0 +1,71 @@
+function SoundVector = BreakTheSilence (num, Freq, Timelength)
+% This is a function with 3 variables
+% Inputs: 
+%           num - sets the number of tones played
+%           Freq - sets the carrier frequency of the tone (Hz)
+%           Timelength - the total length that includes both
+%                        the Tone(s) and Silence_Period
+%
+% Outputs: 
+%           SoundVector - A structure containing vectors of the sound
+%                          waveform for each tone.
+
+%Default Timelength is set to 60s. Default carrier frequency set to 2000Hz. 
+%Default number of tones is set to 2.
+    
+    switch nargin
+        % switch block tests each case until one is true
+        % nargin determines the number of arguments inputted
+        case 2
+            Timelength = 60;
+        case 1
+            Timelength = 60;
+            Freq = 2000;
+        case 0
+            Timelength = 60;
+            Freq = 2000;
+            num = 3;
+    end
+    
+    
+%Sample frequency
+Fs = 200000;
+
+%Initialize the sampling interval (Ts) and the length of the tone (T), 200ms.
+Ts = 1/Fs;
+T = 0:Ts:.2;
+
+NormSin = sin(2*pi*Freq*T);                
+NormSin = NormSin(1:end-1);
+    
+Ramp = round(numel(NormSin)/20);
+RampUp = linspace(0,1,Ramp);
+Hold = ones(1,numel(NormSin)-2*Ramp);
+RampDown = linspace(1,0,Ramp);
+X = [RampUp, Hold, RampDown];
+
+%Apply the trapezoidal waveform to the normal tone.
+Tone = NormSin.*X;
+
+%Define the amount of silence periods based off the num(number of tones)
+Silence_Period = Timelength/(num-1);
+
+%%Create a startsound to avoid any bottleneck in playing the tones when
+%sound begins.
+%Initialize vector for the beginning silence
+Silence = zeros(1, round(Fs*Silence_Period));
+startsound = Silence(1:200000);
+
+%An initial silent tone is played to avoid bottlenecks in audio cues
+%for the first tone played.
+sound(startsound,Fs);
+
+SoundVector = Tone;
+
+    for i = 1:num-1
+        SoundVector = [SoundVector, Silence, Tone];
+        
+    end
+    
+sound(SoundVector,Fs)
+end
