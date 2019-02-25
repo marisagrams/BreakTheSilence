e828bb00da7849ee2a94e0611a4d3028dada0524
marisagrams <43827815+marisagrams@users.noreply.github.com>
2019-02-25 13:35:30 -0800

    Update Silence_Actually.m

--- Silence_Actually.m
+++ Silence_Actually.m
@@ -5,14 +5,14 @@ function SoundVector = BreakTheSilence (num, Freq, Timelength)
 %           Freq - sets the carrier frequency of the tone (Hz)
 %           Timelength - the total length that includes both
 %                        the Tone(s) and Silence_Period
-%
+%   *** num can only exceed timelenght by 1 
 % Outputs: 
 %           SoundVector - A structure containing vectors of the sound
 %                          waveform for each tone.
 
 %Default Timelength is set to 60s. Default carrier frequency set to 2000Hz. 
 %Default number of tones is set to 2.
-    
+
     switch nargin
         % switch block tests each case until one is true
         % nargin determines the number of arguments inputted
@@ -21,13 +21,13 @@ function SoundVector = BreakTheSilence (num, Freq, Timelength)
         case 1
             Timelength = 60;
             Freq = 2000;
-        case 0
-            Timelength = 60;
-            Freq = 2000;
-            num = 3;
+        case 0 
+            Timelength = 10; % 60
+            Freq = 2000; % 2000
+            num = 0; % 3 
     end
-    
-    
+
+
 %Sample frequency
 Fs = 200000;
 
@@ -37,7 +37,7 @@ T = 0:Ts:.2;
 
 NormSin = sin(2*pi*Freq*T);                
 NormSin = NormSin(1:end-1);
-    
+
 Ramp = round(numel(NormSin)/20);
 RampUp = linspace(0,1,Ramp);
 Hold = ones(1,numel(NormSin)-2*Ramp);
@@ -48,9 +48,20 @@ X = [RampUp, Hold, RampDown];
 Tone = NormSin.*X;
 
 %Define the amount of silence periods based off the num(number of tones)
-Silence_Period = Timelength/(num-1);
 
-%%Create a startsound to avoid any bottleneck in playing the tones when
+% no tones for chosen timelenght duration > num = 0, create a silence period 
+% that is the length of time chosen (Timelength)
+if num == 0 
+    Silence_Period = Timelength; 
+end
+
+% else carry on with normal procedure of creating silence periods to be
+% placed inbetween each tone(num)
+if num > 0 
+        Silence_Period = Timelength/(num-1); 
+end 
+
+%Create a startsound to avoid any bottleneck in playing the tones when
 %sound begins.
 %Initialize vector for the beginning silence
 Silence = zeros(1, round(Fs*Silence_Period));
@@ -60,12 +71,25 @@ startsound = Silence(1:200000);
 %for the first tone played.
 sound(startsound,Fs);
 
-SoundVector = Tone;
-
-    for i = 1:num-1
-        SoundVector = [SoundVector, Silence, Tone];
-        
+% if no tones (num) were chosen > the sound vector is just the duration (or timelength)
+% chosen by user > the duration of Silence
+if num == 0
+    for i = 1:num+1 % i might have to re look at this <<<<<<<<<<
+        SoundVector = Silence; 
+        % if you plot SoundVector (or ans that apeares in the workspace) at the end of running this code, it
+        % will give you a visulaization of the period of silence that occured 
     end
-    
-sound(SoundVector,Fs)
+end
+
+% however if a number of tones (num) were chosen then SoundVector becomes
+% the component parts determined by the inputs: num, Freq, and Timelength 
+if num > 0
+    SoundVector = Tone; % just a portion of the old code moved into this 
+    for i = 1:num-1
+        SoundVector = [SoundVector, Silence, Tone]; 
+    end
+
+end
+   sound(SoundVector,Fs);
+
 end
