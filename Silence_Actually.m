55459788594975c4f94f86346e9c235448d1ef80
marisagrams <43827815+marisagrams@users.noreply.github.com>
2019-02-26 19:21:51 -0800

    Final Draft

--- Silence_Actually.m
+++ Silence_Actually.m
@@ -4,8 +4,7 @@ function SoundVector = BreakTheSilence (num, Freq, Timelength)
 %           num - sets the number of tones played
 %           Freq - sets the carrier frequency of the tone (Hz)
 %           Timelength - the total length that includes both
-%                        the Tone(s) and Silence_Period
-%   *** num can only exceed timelenght by 1 
+%                        the Tone(s) and Silence_Period 
 % Outputs: 
 %           SoundVector - A structure containing vectors of the sound
 %                          waveform for each tone.
@@ -22,9 +21,9 @@ function SoundVector = BreakTheSilence (num, Freq, Timelength)
             Timelength = 10;
             Freq = 2000;
         case 0 
-            Timelength = 10;
+            Timelength = 13;
             Freq = 2000;
-            num = 0;  
+            num = 1;  
     end
 
 
@@ -34,6 +33,9 @@ Fs = 200000;
 %Initialize the sampling interval (Ts) and the length of the tone (T), 200ms.
 Ts = 1/Fs;
 T = 0:Ts:.2;
+
+TotalTime = 0:Ts:Timelength;                            %%%%%%%%%%%%%%%%%%%%
+TotalTime = TotalTime(1:end-1);                         %%%%%%%%%%%%%%%%%%%% this variable is to compare against the ans gotten at the end of running the function. ans should be this long 
 
 NormSin = sin(2*pi*Freq*T);                
 NormSin = NormSin(1:end-1);
@@ -47,6 +49,8 @@ X = [RampUp, Hold, RampDown];
 %Apply the trapezoidal waveform to the normal tone.
 Tone = NormSin.*X;
 
+ToneLength_B = 2*length(Tone);                                    %%%%%%%%%%%%%%%% tone length at beginning: this is for the SoundVector portion. every first iteration of the making of sound vector, this value will be subtracted from silence
+ToneLength_E = length(Tone);                                      %%%%%%%%%%%%%%%% tone length at end: every iteration after first itteration of SoundVector
 %Define the amount of silence periods based off the num(number of tones)
 
 % no tones for chosen timelength duration > num = 0, create a silence period 
@@ -55,15 +59,15 @@ if num == 0
     Silence_Period = Timelength; 
 elseif num == 1
     Silence_Period = Timelength;
-    response = input('Press 1 if you would like the tone at the beginning\nPress 2 if you would like the tone at the end','s');
+    response = input('Press 1 if you would like the tone at the beginning or\nPress 2 if you would like the tone at the end: ','s');
     Decision = 0;
         if strcmp(response, '1')==1
             Decision = 1;
         elseif strcmp(response, '2')==1
             Decision = 2;
-        elseif strcmp(response, '1')==0 || strcmp(response, '2')==0
-            disp('Please choose 1 or 2')
-            %It ends here after though, I need it to loop again
+        elseif strcmp(response, '1')==0 || strcmp(response, '2')==0  
+            warning('You did not choose "1" or "2". Assigning a value of 2.');
+            Decision = 2;
         end
 elseif num > 0
         Silence_Period = Timelength/(num-1); 
@@ -72,7 +76,11 @@ end
 %Create a startsound to avoid any bottleneck in playing the tones when
 %sound begins.
 %Initialize vector for the beginning silence
-Silence = zeros(1, round(Fs*Silence_Period));
+Silence = zeros(1, round(Fs*Silence_Period)); 
+
+Silence_B = zeros(1, round((Fs*Silence_Period)-ToneLength_B));       %%%%%%%%%%%%%%%%%%%%%%%%%% silence portion for first iteration of SoundVector
+Silence_E = zeros(1, round((Fs*Silence_Period)-ToneLength_E));       %%%%%%%%%%%%%%%%%%%%%%%%%% silence portion for every iteration after first iteration of SoundVector 
+
 startsound = Silence(1:200000);
 
 %An initial silent tone is played to avoid bottlenecks in audio cues
@@ -82,20 +90,24 @@ sound(startsound,Fs);
 for i = num
     if num == 0
          SoundVector = Silence; 
-         sound(SoundVector,Fs);
     elseif num == 1
          SoundVector = Tone;
             if Decision == 1 
+                SoundVector = [SoundVector, Silence_E];
                 sound(SoundVector,Fs);
             elseif Decision == 2
-                SoundVector = [Silence, SoundVector]; 
-                sound(SoundVector,Fs);
+                SoundVector = [Silence_E, SoundVector]; 
             end
     elseif num > 0 
         SoundVector = Tone; % just a portion of the old code moved into this 
+       % Silence = Silence - Tone; 
         for i = 1:num-1
-            SoundVector = [SoundVector, Silence, Tone]; 
-            sound(SoundVector,Fs);
+            if i == 1                                                  %%%%%%%%
+                SoundVector = [SoundVector, Silence_B, Tone];          %%%%%%%%
+            else                                                       %%%%%%%%
+                SoundVector = [SoundVector,Silence_E, Tone];           %%%%%%%%
+            end                                                        %%%%%%%%
         end
     end
+    sound(SoundVector,Fs);
 end
